dfedd849350518158bf0ba0e5bb3f6835c4549b1
MaxXxim <120455884+MaximStroev2004@users.noreply.github.com>
2024-04-14 15:31:20 +0300

    app

---
+++ coctalis/app/build.gradle.kts
@@ -0,0 +1,61 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlin.kapt)
+}
+
+android {
+    namespace = "com.arshapshap.surftraineetask"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.arshapshap.surftraineetask"
+        minSdk = 26
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kapt {
+        arguments {
+            arg("room.schemaLocation", "$projectDir/schemas")
+        }
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.android.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.bundles.navigation)
+
+    implementation(libs.bundles.room)
+    kapt(libs.bundles.room.kapt)
+
+    implementation(libs.bundles.dagger)
+    kapt(libs.bundles.dagger.kapt)
+}
